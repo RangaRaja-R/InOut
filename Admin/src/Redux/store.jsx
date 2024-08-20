bb79fb4100456ffbfa474f802132d28058c9d6d2
gowtham-cyber-max <gowtham4262472@gmail.com>
2024-08-20 21:36:55 +0530

    Login started

--- Admin/src/Redux/store.jsx
+++ Admin/src/Redux/store.jsx
@@ -1,6 +1,7 @@
 import UserListReducer from "./reducers/UserListReducer";
+import AuthReducer from './reducers/AuthReducer';
 import { configureStore } from '@reduxjs/toolkit'
-export const API_URL = "";
+export const API_URL = "http://localhost:8000";
 
 export default configureStore({
     reducer:{
