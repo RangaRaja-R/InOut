7572b385bbe9b9ccfb7b7f37e65e9c70a78be59e
vainavibalasubramanian <vainavibalasubramanian@gmail.com>
2024-10-16 14:41:24 +0530

    sidebar

--- Admin/src/Pages/SideBar.jsx
+++ Admin/src/Pages/SideBar.jsx
@@ -40,8 +40,8 @@ function SideBar() {
           <h3 onClick={navToEmployeList}>Employee List</h3>
         </div>
         <div className='log-out'>
+          <h2>Log out</h2>
           <svg onClick={logOutHandle} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" /></svg>
-          <h3>Log out</h3>
         </div>
       </div>
     </div>
